52dfa26ba3de0ba18004463bd524304e3a47bb56
Soham Haldar <sohamhaldar25@gmail.com>
2025-09-14 19:01:45 +0530

    fixing ribbons for mobile section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,16 @@ function Landing() {
         <h1 className='text-7xl font-bold text-black'>Planify</h1>
         <p className='text-2xl text-center text-black'>Plan your weekends the right way</p>
         <Link href="/planner" className='hover:cursor-pointer hover:scale-110'><Button className='text-xl font-semibold mt-10 '>Plan Now</Button></Link>
-        <InfiniteRibbon rotation={-45} className="absolute bottom-36 -right-36 bg-[#805c24] text-white py-2 whitespace-nowrap overflow-hidden w-[800px]">
+        <InfiniteRibbon rotation={-45} className="absolute bottom-20 md:bottom-36  -right-36 bg-[#805c24] text-white py-2 whitespace-nowrap overflow-hidden w-[800px]">
             Monday &nbsp; &nbsp; &nbsp; &nbsp; Tuesday &nbsp; &nbsp; &nbsp; &nbsp; Wednesday &nbsp; &nbsp; &nbsp; &nbsp; Thursday &nbsp; &nbsp; &nbsp; &nbsp; Friday &nbsp; &nbsp; &nbsp; &nbsp; Saturday &nbsp; &nbsp; &nbsp; &nbsp; Sunday &nbsp; &nbsp; &nbsp; &nbsp;
         </InfiniteRibbon>
-        <InfiniteRibbon rotation={-45} className="absolute top-36 -left-36 bg-black text-white py-2 whitespace-nowrap overflow-hidden w-[800px]">
+        <InfiniteRibbon rotation={-45} className="absolute top-20 md:top-36 -left-36 bg-black text-white py-2 whitespace-nowrap overflow-hidden w-[800px]">
             Monday &nbsp; &nbsp; &nbsp; &nbsp; Tuesday &nbsp; &nbsp; &nbsp; &nbsp; Wednesday &nbsp; &nbsp; &nbsp; &nbsp; Thursday &nbsp; &nbsp; &nbsp; &nbsp; Friday &nbsp; &nbsp; &nbsp; &nbsp; Saturday &nbsp; &nbsp; &nbsp; &nbsp; Sunday &nbsp; &nbsp; &nbsp; &nbsp;
         </InfiniteRibbon>
-        <InfiniteRibbon rotation={45} className="absolute top-36 -right-36 bg-black text-white py-2 whitespace-nowrap overflow-hidden w-[800px]">
+        <InfiniteRibbon rotation={45} className="absolute top-20 md:top-36 -right-36 bg-black text-white py-2 whitespace-nowrap overflow-hidden w-[800px]">
             Monday &nbsp; &nbsp; &nbsp; &nbsp; Tuesday &nbsp; &nbsp; &nbsp; &nbsp; Wednesday &nbsp; &nbsp; &nbsp; &nbsp; Thursday &nbsp; &nbsp; &nbsp; &nbsp; Friday &nbsp; &nbsp; &nbsp; &nbsp; Saturday &nbsp; &nbsp; &nbsp; &nbsp; Sunday &nbsp; &nbsp; &nbsp; &nbsp;
         </InfiniteRibbon>
-        <InfiniteRibbon rotation={45} className="absolute bottom-36 -left-36 bg-amber-700 text-white py-2 whitespace-nowrap overflow-hidden w-[800px]">
+        <InfiniteRibbon rotation={45} className="absolute bottom-20 md:bottom-36 -left-36 bg-amber-700 text-white py-2 whitespace-nowrap overflow-hidden w-[800px]">
             Monday &nbsp; &nbsp; &nbsp; &nbsp; Tuesday &nbsp; &nbsp; &nbsp; &nbsp; Wednesday &nbsp; &nbsp; &nbsp; &nbsp; Thursday &nbsp; &nbsp; &nbsp; &nbsp; Friday &nbsp; &nbsp; &nbsp; &nbsp; Saturday &nbsp; &nbsp; &nbsp; &nbsp; Sunday &nbsp; &nbsp; &nbsp; &nbsp;
         </InfiniteRibbon>
         
